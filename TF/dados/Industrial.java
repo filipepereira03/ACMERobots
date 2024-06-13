8632210fc085af7406a13f4c4524b92f8a30d386
chiqu <filipe.pereira002@edu.pucrs.br>
2024-06-13 03:50:30 -0300

    Mudança na locação

--- TF/dados/Industrial.java
+++ TF/dados/Industrial.java
@@ -4,13 +4,13 @@ public class Industrial extends Robo {
 
 	private String setor;
 
-	public Industrial(int id, String modelo, double valorDiario, String setor) {
-		super(id, modelo, valorDiario);
+	public Industrial(int id, String modelo, String setor) {
+		super(id, modelo, 90);
 		this.setor = setor;
     }
 
 	@Override
 	public double calculaLocacao(int dias) {
-		return dias * 90.00;
+		return getValorDiario() * dias;
 	}
 }
